f4895142ee1199696ea7959923c8910618cd6208
SorenAndersen1 <andesore@oregonstate.edu>
2020-06-01 17:09:03 -0700

    Figured out how to anything with handlebars

--- server.js
+++ server.js
@@ -1,16 +1,50 @@
+var path = require('path');
 var express = require('express');
+var exphbs = require('express-handlebars');
+
 var app = express();
+var port = 3001;
 
-var logger = require('./logger');
+// var peopleData = require('./peopleData');
+// console.log("== peopleData", peopleData);
 
-app.use(logger);
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
-
-app.get('/', function (req, res, next) {
-  res.status(200).sendFile(__dirname + '/villagers/villagers.html');
+app.get('/', function(req, res) {
+    res.render('layouts/main')
 });
 
-app.listen(3000, function () {
-  console.log("== Server is listening on port 3000");
+app.get('/island', function(req, res) {
+    res.render('island')
+});
+
+app.get('/fruitFlowers', function(req, res) {
+    res.render('fruitFlowers')
+});
+
+app.get('/homepage', function(req, res) {
+    res.render('homepage')
+});
+
+app.get('/login', function(req, res) {
+    res.render('login')
+});
+
+app.get('/villagers', function(req, res) {
+    res.render('villagers')
+});
+
+
+
+
+
+app.use('/island', express.static(__dirname + 'island' ));
+
+app.listen(port, function (err) {
+  if (err) {
+    throw err;
+  }
+  console.log("== Server listening on port", port);
 });
