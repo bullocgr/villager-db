6d57fff28a60425d650e6df74d2814c036c47b28
Reed <503beav@gmail.com>
2020-06-06 17:17:17 -0700

    Completed ability to add villagers,fruits,flowers

--- serverside JS/fruitFlowers.js
+++ serverside JS/fruitFlowers.js
@@ -28,7 +28,7 @@ module.exports = function(){
     router.get('/', function(req, res){
       var callbackCount = 0
       var context = {};
-      context.jsscripts = ["addFlowersToDB"];
+      context.jsscripts = ["fruitsFlowers.js"];
       var mysql = req.app.get('mysql');
       getFruits(res, mysql, context, complete);
       getFlowers(res, mysql, context, complete);
@@ -53,21 +53,38 @@ module.exports = function(){
 
 //    }
 
+    router.post('/Fruit', function(req, res) {
+      console.log("in post!")
+      var mysql = req.app.get('mysql');
+      var sql = "Insert into fruit values (?,?)";
+      var inserts = [req.body.ffruit_name, req.body.fprice];
+      sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+        if(error){
+            console.log(JSON.stringify(error))
+            res.write(JSON.stringify(error));
+            res.end();
+        }else{
+            res.redirect('/fruitFlowers');
+        }
+      });
+    });
 
-//    router.post('/flowers', function(req, res) {
-//      var mysql = req.app.get('mysql');
-//      var sql = "Insert into fruit values (?,?)";
-//      var inserts = [req.body.fflower_name, req.body.fflower_color];
-//      sql = mysql.pool.query(sql, inserts, function(error, results, fields){
-//        if(error){
-//            console.log(JSON.stringify(error))
-//            res.write(JSON.stringify(error));
-//            res.end();
-//        }else{
-//            res.redirect('/fruitFlowers');
-//        }
-//      });
-//    });
+
+    router.post('/Flowers', function(req, res) {
+      console.log("in post!")
+      var mysql = req.app.get('mysql');
+      var sql = "Insert into flower values (?,?)";
+      var inserts = [req.body.fflower_name, req.body.fflower_color];
+      sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+        if(error){
+            console.log(JSON.stringify(error))
+            res.write(JSON.stringify(error));
+            res.end();
+        }else{
+            res.redirect('/fruitFlowers');
+        }
+      });
+    });
 
      return router;
      }();
