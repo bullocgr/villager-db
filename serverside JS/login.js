59fc49574994bad96d71196512200abcfe495fc8
Grace Bullock <bullocgr@oregonstate.edu>
2020-06-09 19:11:32 -0700

    changed the login page

--- serverside JS/login.js
+++ serverside JS/login.js
@@ -2,61 +2,43 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();  
 
-
-
-    function getAccount(req, res, mysql, context){
-      var query = "SELECT id, username FROM player WHERE password = ? AND username = ?";
-      var inserts = [req.body.fpassword, req.body.fusername];
-      console.log(inserts);
-      mysql.pool.query(query, inserts, function(error, results, fields){
+    
+    router.post('/AddAccount', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "INSERT INTO player VALUES (?, ?, ?, (SELECT name FROM island WHERE name = ?));";
+        var inserts = [req.body.fyourname, req.body.fusername, req.body.fpassword, req.body.fislandName];
+        console.log(inserts);
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
             if(error){
+                console.log(JSON.stringify(error))
                 res.write(JSON.stringify(error));
-                res.redirect('/login');
                 res.end();
+            }else{
+                res.redirect('/login');
             }
-            else if (results[0] && results[0].password == req.fpassword){
-            context.account = results;
-                    var accountSuccess = {
-          id: results[0].id,
-          
-        }
-          console.log(accountSuccess);
-          console.log("got here2");
-          res.redirect('/island/' + results[0].id);
-
-             
-            }
-            else{
-                     res.write(JSON.stringify(error));
-                     res.end();
-            }
-
         });
-
-    }
-    
-    // router.post('/Addvill', function(req, res){
-    //     var mysql = req.app.get('mysql');
-    //     var sql = "INSERT INTO villager (image, name, personality, animal) VALUES (?,?,?,?)";
-    //     var inserts = [req.body.fimage, req.body.fname, req.body.fpersonality, req.body.fanimal];
-    //     console.log(inserts);
-    //     sql = mysql.pool.query(sql,inserts,function(error, results, fields){
-    //         if(error){
-    //             console.log(JSON.stringify(error))
-    //             res.write(JSON.stringify(error));
-    //             res.end();
-    //         }else{
-    //             res.redirect('/villagers');
-    //         }
-    //     });
-    // });
-
-    
-    router.post('/', function(req, res){
-    var mysql = req.app.get('mysql');
-    var context = {};
-    getAccount(req, res, mysql, context);
     });
+
+    router.post('/AddIsland', function(req, res){
+      var mysql = req.app.get('mysql');
+      var sql = "INSERT INTO island (name, location, start_date) VALUES (?,?,?)";
+      var inserts = [req.body.fislandName, req.body.flocation, req.body.fstartDate];
+      console.log(inserts);
+      sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+          if(error){
+              console.log(JSON.stringify(error))
+              res.write(JSON.stringify(error));
+              res.end();
+          }else{
+              res.redirect('/login');
+          }
+      });
+  });
+
+  function updatePlayerID(req, res, mysql, context) {
+
+  }
+
 
      return router;
      }();
